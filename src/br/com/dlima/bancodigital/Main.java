cc7262695acf93906505ade74b732055f5a74a42
Marcos Lima <marcosanthony21@hotmail.com>
2022-03-01 01:48:10 -0300

    Banco Digital com POO em Java

---
+++ src/br/com/dlima/bancodigital/Main.java
@@ -0,0 +1,21 @@
+package br.com.dlima.bancodigital;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Cliente cliente = new Cliente();
+		cliente.setNome("Luffy");
+		
+		Conta cc = new ContaCorrente(cliente);
+		Conta poupanca = new ContaPoupanca(cliente);
+
+		cc.depositar(100);
+		cc.transferir(100, poupanca);
+		
+		cc.imprimirExtrato();
+		poupanca.imprimirExtrato();
+
+	}
+
+}
